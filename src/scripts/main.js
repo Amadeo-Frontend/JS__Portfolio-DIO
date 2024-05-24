54845abcd3d880cf2f5168c2d67973cd0b32d39d
Amadeo Bon <amadeosph@outlook.com>
2024-05-24 01:47:44 -0300

    add acordeon and dio logo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-window.console.log("olá");
+function toggleAccordion(trigger) {
+    const accordion = trigger.parentElement;
+    const arrow = trigger.querySelector('.arrow');
+    const isActive = accordion.classList.contains('active');
+  
+    accordion.classList.toggle('active');
+  
+    // Alternar a imagem da seta
+    if (isActive) {
+      arrow.src = './public/images/trigger-up.svg';
+    } else {
+      arrow.src = './public/images/trigger-down.svg';
+    }
+  }
+  
