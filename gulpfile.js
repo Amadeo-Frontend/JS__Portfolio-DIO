a4f346fc8bb8bc91b51f030a1b8f7b0a613155be
Amadeo Bon <amadeosph@outlook.com>
2024-05-24 02:55:00 -0300

    add animations

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,24 @@ const uglify = require("gulp-uglify");
 
 function scripts() {
   return gulp
-   .src("./src/scripts/*.js")
-   .pipe(uglify())
-   .pipe(gulp.dest("./public/js"));
+    .src("./src/scripts/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./public/js"));
 }
 
 function styles() {
   return gulp
-   .src("./src/styles/*.scss")
-   .pipe(sass({ outputStyle: "compressed" }))
-   .pipe(gulp.dest("public/css"));
+    .src("./src/styles/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("public/css"));
 }
 
 function images() {
   return gulp
-   .src("./src/images/**/*")
-   .pipe(imagemin())
-   .pipe(gulp.dest("public/images"));
+    .src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("public/images"));
 }
-
 
 exports.default = gulp.parallel(styles, images, scripts);
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,14 @@
 function toggleAccordion(trigger) {
-    const accordion = trigger.parentElement;
-    const arrow = trigger.querySelector('.arrow');
-    const isActive = accordion.classList.contains('active');
-  
-    accordion.classList.toggle('active');
-  
-    // Alternar a imagem da seta
-    if (isActive) {
-      arrow.src = './public/images/trigger-up.svg';
-    } else {
-      arrow.src = './public/images/trigger-down.svg';
-    }
+  const accordion = trigger.parentElement;
+  const arrow = trigger.querySelector(".arrow");
+  const isActive = accordion.classList.contains("active");
+
+  accordion.classList.toggle("active");
+
+  // Alternar a imagem da seta
+  if (isActive) {
+    arrow.src = "./public/images/trigger-up.svg";
+  } else {
+    arrow.src = "./public/images/trigger-down.svg";
   }
-  
+}
