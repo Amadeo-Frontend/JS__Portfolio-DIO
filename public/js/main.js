91571134345a7b8f764e20b1450e807075922a97
Amadeo Bon <amadeosph@outlook.com>
2024-05-24 22:08:16 -0300

    add portfolio

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-function toggleAccordion(e){var i=e.parentElement,e=e.querySelector(".arrow"),t=i.classList.contains("active");i.classList.toggle("active"),e.src=t?"./public/images/trigger-up.svg":"./public/images/trigger-down.svg"}
+function toggleAccordion(e) {
+  var i = e.parentElement,
+    e = e.querySelector(".arrow"),
+    t = i.classList.contains("active");
+  i.classList.toggle("active"),
+    (e.src = t
+      ? "./public/images/trigger-up.svg"
+      : "./public/images/trigger-down.svg");
+}
