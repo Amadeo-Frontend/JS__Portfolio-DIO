3f7c1fd71ac145aa1cb06ae0b24b716064a23933
Amadeo Bon <amadeosph@outlook.com>
2024-05-26 20:14:59 -0300

    add skills dinamic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,7 +17,37 @@ function updateProfileData(profileData) {
   email.href = `mailto:${profileData.email}`;
 }
 
+function updateSoftSkillsData(profileData) {
+  // Verifica se o elemento existe antes de tentar acessá-lo
+  const softSkillsElement = document.getElementById("profile.skills.softSkills");
+
+  if (softSkillsElement) {
+    // Limpa o conteúdo anterior do elemento
+    softSkillsElement.innerHTML = '';
+
+    // Atualiza o conteúdo com novas habilidades suaves
+    softSkillsElement.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
+  }
+}
+
+function updateHardSkillsData(profileData) {
+  // Verifica se o elemento existe antes de tentar acessá-lo
+  const hardSkillsElement = document.getElementById("profile.skills.hardSkills");
+
+  if (hardSkillsElement) {
+    // Limpa o conteúdo anterior do elemento
+    hardSkillsElement.innerHTML = '';
+
+    // Atualiza o conteúdo com novas habilidades suaves
+    hardSkillsElement.innerHTML = profileData.skills.hardSkills.map(skill => 
+      `  <li><img src='${skill.logo}' alt='${skill.name}' title='${skill.name}'/></li>
+      `).join('');
+  }
+}
+
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileData(profileData);
+  updateSoftSkillsData(profileData);
+  updateHardSkillsData(profileData);
 })();
